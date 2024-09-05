0a4be8ee4888380f001478f146422ae5eb4e89dc
Sebastian Döll <sebastian@katallaxie.dev>
2024-09-05 19:33:39 +0000

    chore(dev): python feature

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 # This script is executed after the creation of a new project.
+
+# asdf
+# git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+. "$HOME/.asdf/asdf.sh"
