0d23cbcde2bb5a1c45eddd981b4ac37939e573b9
Sebastian Döll <sebastian@katallaxie.dev>
2024-09-18 11:58:59 +0000

    chore: bump eventing-natss to "v1.15.0"

--- staging/eventing/upgrade_nats.sh
+++ staging/eventing/upgrade_nats.sh
@@ -15,19 +15,21 @@ NATS_TAG=1.15.0
 NATS_URL=https://github.com/knative-extensions/eventing-natss/releases/download/knative-v${NATS_TAG}
 
 # Get all files, auto-apply PodDisruptionBudget patches
-curl -sSL ${NATS_URL}/eventing-natss.yaml > charts/nats/templates/eventing-natss.yaml
-curl -sSL ${NATS_URL}/eventing-jsm.yaml > charts/nats/templates/eventing-jsm.yaml
+curl -sSL ${NATS_URL}/eventing-natss.yaml > charts/eventing-natss/templates/eventing-natss.yaml
+curl -sSL ${NATS_URL}/eventing-jsm.yaml > charts/eventing-jsm/templates/eventing-jsm.yaml
 
 # Bump app version
 sed "s/appVersion:.*/appVersion: \"v${NATS_TAG}\"/g" Chart.yaml > Chart.yaml.temp
-sed "s/appVersion:.*/appVersion: \"v${NATS_TAG}\"/g" charts/nats/Chart.yaml > charts/nats/Chart.yaml.temp
+sed "s/appVersion:.*/appVersion: \"v${NATS_TAG}\"/g" charts/eventing-natss/Chart.yaml > charts/eventing-natss/Chart.yaml.temp
+sed "s/appVersion:.*/appVersion: \"v${NATS_TAG}\"/g" charts/eventing-jsm/Chart.yaml > charts/eventing-jsm/Chart.yaml.temp
 mv Chart.yaml.temp Chart.yaml
-mv charts/nats/Chart.yaml.temp charts/nats/Chart.yaml
+mv charts/eventing-natss/Chart.yaml.temp charts/eventing-natss/Chart.yaml
+mv charts/eventing-jsm/Chart.yaml.temp charts/eventing-jsm/Chart.yaml
 
 # Commit changes
 git add .
 git commit -am "chore: bump eventing-natss to \"v${NATS_TAG}\""
 
-# Finish
+# # Finish
 echo "Done upgrading eventing-natss!"
 echo "Please remember to bump version numbers in Chart and sub-Charts manually"
